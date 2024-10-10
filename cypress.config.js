2552a1e01e5e8572695c457696ee7acb535c6756
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-10-10 10:22:43 +0200

    Fix homepage

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://www.fiskeridir.no',  // Angi URL for lokal installasjon
-    //baseUrl: 'http://localhost:3000/nohttp://localhost:8080/admin/site/preview/default/draft/no',  // Angi URL for lokal installasjon
+    //baseUrl: 'https://www.fiskeridir.no',  // Angi URL for lokal installasjon
+    baseUrl: 'http://localhost:8080/admin/site/preview/default/draft/no/Registre',  // Angi URL for lokal installasjon
     viewportWidth: 1280,                  // Sett skjermstørrelse (bredde)
     viewportHeight: 720,                  // Sett skjermstørrelse (høyde)
+    experimentalSessionAndOrigin: true, // Aktiverer eksperimentelle funksjoner
     setupNodeEvents(on, config) {
       // Legg til event-håndterere hvis nødvendig
     },
@@ -17,5 +18,6 @@ module.exports = defineConfig({
     html: true,
     json: false,
   },
+  "experiment": true
 
 });
--- cypress/e2e/Menu.cy.js
+++ cypress/e2e/Menu.cy.js
@@ -1,10 +1,10 @@
 describe('Hjemmeside tester', () => {
     it('Verifiser at hjemmesiden laster korrekt', () => {
-        cy.visit('https://www.fiskeridir.no/');
-        cy.url().should('contains', 'https://www.fiskeridir.no/Yrkesfiske');
-        cy.url().should('contains', 'https://www.fiskeridir.no/Akvakultur');
-        cy.url().should('contains', 'https://www.fiskeridir.no/Areal-og-miljo');
-        cy.url().should('contains', 'https://www.fiskeridir.no/Fritidsfiske');
-        cy.url().should('contains', 'https://www.fiskeridir.no/Turistfiske');
+        cy.visit('/');
+        cy.url().should('contains', 'Yrkesfiske');
+        cy.url().should('contains', 'Akvakultur');
+        cy.url().should('contains', 'Areal-og-miljo');
+        cy.url().should('contains', 'Fritidsfiske');
+        cy.url().should('contains', 'Turistfiske');
     });
 });
