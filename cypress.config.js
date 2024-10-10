086b2bf0cd94e8de402328525c229a77c7b771b1
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-10-10 13:50:20 +0200

    Fix URL enonic master

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     //baseUrl: 'https://www.fiskeridir.no',  // Angi URL for lokal installasjon
-    baseUrl: 'http://localhost:8080/admin/site/preview/default/draft/no',  // Angi URL for lokal installasjon
+    baseUrl: 'http://localhost:8080/site/default/master/no',  // Angi URL for lokal installasjon
     viewportWidth: 1280,                  // Sett skjermstørrelse (bredde)
     viewportHeight: 720,                  // Sett skjermstørrelse (høyde)
     experimentalSessionAndOrigin: true, // Aktiverer eksperimentelle funksjoner
--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,10 +1,10 @@
 describe('Hjemmeside tester', () => {
     it('Verifiser at hjemmesiden laster korrekt', () => {
    
-      cy.login('usuario', 'contraseña');
-      cy.visit('/');
+      //cy.login('username', 'password');
+      //cy.visit('/');
 
-      cy.visit('/');  // Dette tilsvarer å besøke 'http://localhost:3000/nohttp://localhost:8080/admin/site/preview/default/draft/no'
+      cy.visit('/');  // Dette tilsvarer å besøke 'http://localhost:8080/site/default/master/no'
       cy.contains('Fiskeridirektoratet').should('be.visible');  // Sjekk at teksten "Fiskeridirektoratet" er synlig
       
     });
