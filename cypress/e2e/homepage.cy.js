4004a8eccb400b40faf29fe6b19c8f6e61274ea9
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-10-11 10:19:13 +0200

    Home page testr

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,4 +1,4 @@
-describe('Hjemmeside tester', () => {
+describe('Hjemmeside norsk test', () => {
     it('Verifiser at hjemmesiden laster korrekt', () => {
    
       //cy.login('username', 'password');
@@ -6,6 +6,18 @@ describe('Hjemmeside tester', () => {
 
       cy.visit('/');  // Dette tilsvarer å besøke 'http://localhost:8080/site/default/master/no'
       cy.contains('Fiskeridirektoratet').should('be.visible');  // Sjekk at teksten "Fiskeridirektoratet" er synlig
+      
+    });
+  });
+
+  describe('Hjemmeside engelsk test', () => {
+    it('Verifiser at hjemmesiden laster korrekt', () => {
+   
+      //cy.login('username', 'password');
+      //cy.visit('/');
+
+      cy.visit('http://localhost:8080/site/default/master/eng');  // Dette tilsvarer å besøke 'http://localhost:8080/site/default/master/eng'
+      cy.contains('Directorate of Fisheries').should('be.visible');  // Sjekk at teksten "Directorate of Fisheries" er synlig
       
     });
   });
