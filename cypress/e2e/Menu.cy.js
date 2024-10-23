677fdb51c373941f64db58a272767653d0d32fe6
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-10-23 10:51:07 +0200

    Expand test under meny norsk

--- cypress/e2e/Menu.cy.js
+++ cypress/e2e/Menu.cy.js
@@ -20,11 +20,64 @@ describe('Hjemmeside tester no', () => {
         cy.visit('/');
         cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/Rapportering-paa-havet');
         cy.get('[class="btn-nav__title"]').eq(0).click();
+        cy.contains('Rapportering på havet');        
+        
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/Rapportering-ved-landing');
+        cy.get('[class="btn-nav__title"]').eq(1).click();
+        cy.contains('Rapportering ved landing');  
+        
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/Regelverk-og-reguleringer');
+        cy.get('[class="btn-nav__title"]').eq(2).click();
+        cy.contains('Regelverk og reguleringer');  
 
-        //cy.get('.btn.btn--searchsubmit').eq(0).click()
-        cy.contains('Rapportering på havet');           
-              
-		
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/Kontroll');
+        cy.get('[class="btn-nav__title"]').eq(3).click();
+        cy.contains('Kontroll');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/omsetning-fisk');
+        cy.get('[class="btn-nav__title"]').eq(4).click();
+        cy.contains('Omsetning');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/Registre-og-skjema');
+        cy.get('[class="btn-nav__title"]').eq(5).click();
+        cy.contains('Registre og skjema - yrkesfiske');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/Havmiljoe');
+        cy.get('[class="btn-nav__title"]').eq(6).click();
+        cy.contains('Havmiljø');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/fangstid');
+        cy.get('[class="btn-nav__title"]').eq(7).click();
+        cy.contains('FangstID');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/Tall-og-analyse');
+        cy.get('[class="btn-nav__title"]').eq(8).click();
+        cy.contains('Tall og analyse: Yrkesfiske');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/Kartverktoyet');
+        cy.get('[class="btn-nav__title"]').eq(9).click();
+        cy.contains('Kartverktøyet');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/Dokumenter');
+        cy.get('[class="btn-nav__title"]').eq(10).click();
+        cy.contains('Dokumenter - yrkesfiske');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/Tema');
+        cy.get('[class="btn-nav__title"]').eq(11).click();
+        cy.contains('Tema');  
+
+	
       });
 
 
