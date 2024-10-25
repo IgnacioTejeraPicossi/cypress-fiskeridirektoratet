58b3cd9c9579686922fc2f359a82cb221cc8088e
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-10-25 10:08:08 +0200

    Add Ipad and mobil testing

--- cypress/e2e/visores.cy.js
+++ cypress/e2e/visores.cy.js
@@ -4,10 +4,35 @@ describe('Responsivitetstester', () => {
       cy.visit('/');
       cy.get('nav').should('be.visible');
     });
+    
+    it('Test på iPhone 15', () => {
+      cy.viewport(1179, 2556); // Øppløsning de iPhone 15
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+    });
+  
+    it('Test på Samsung Galaxy S22', () => {
+      cy.viewport(1080, 2340); // Øppløsning Galaxy S22
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+    });
   
     it('Test i skrivebordsoppløsning', () => {
       cy.viewport(1280, 720);  // Skrivebordsoppløsning
       cy.visit('/');
       cy.get('nav').should('be.visible');
     });
+    
+    it('Test på iPad', () => {
+      cy.viewport('ipad-2'); 
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+    });
+
+    it('Test på iPad', () => {
+      cy.viewport('ipad-mini');
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+    });
+
   });
