7cdae0810b585a888e9191a50a26f6503d98b59a
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-12-05 14:53:54 +0100

    Add Areal og miljo  test

--- cypress/e2e/Menuno.cy.js
+++ cypress/e2e/Menuno.cy.js
@@ -12,7 +12,100 @@ describe('Hjemmeside tester no', () => {
       
     });
 
-    it('Test NO under menu Yrkesfiske', () => {
+    
+      it('Test NO under menu Akvakultur', () => {
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur');
+        cy.contains('Akvakultur').should('be.visible');
+           /* ==== Test meny opsjon ==== */
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Tildeling-og-tillatelser');
+        cy.get('[class="btn-nav__title"]').eq(0).click();
+        cy.contains('Tildeling og tillatelser');        
+        
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Drift-og-tilsyn');
+        cy.get('[class="btn-nav__title"]').eq(1).click();
+        cy.contains('Drift og tilsyn');  
+        
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Delt-kunnskap-og-erfaring');
+        cy.get('[class="btn-nav__title"]').eq(2).click();
+        cy.contains('Delt kunnskap og erfaring');  
+
+        //cy.visit('/');
+        //cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Dokumenter');
+        //cy.get('[class="btn-nav__title"]').eq(3).click();
+        //cy.contains('Dokumenter - akvakultur');  
+
+        //cy.visit('/');
+        //cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Registre-og-skjema');
+        //cy.get('[class="btn-nav__title"]').eq(4).click();
+        //cy.contains('Registre og skjema');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Tall-og-analyse');
+        cy.get('[class="btn-nav__title"]').eq(5).click();
+        cy.contains('Tall og analyse');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Tema');
+        cy.get('[class="btn-nav__title"]').eq(6).click();
+        cy.contains('Tema');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Akvakulturloven-og-forskrifter');
+        cy.get('[class="btn-nav__title"]').eq(7).click();
+        cy.contains('Akvakulturloven og forskrifter');  
+      });
+
+      it('Test NO under menu Areal og miljø', () => {
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Areal-og-miljo');
+        cy.contains('Areal og miljø').should('be.visible');
+           /* ==== Test meny opsjon ==== */
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Areal-og-miljo/Marine-verneomraader-MPA');
+        cy.get('[class="btn-nav__title"]').eq(0).click();
+        cy.contains('Marine verneområde');        
+        
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Areal-og-miljo/Marin-forsoepling');
+        cy.get('[class="btn-nav__title"]').eq(1).click();
+        cy.contains('Marin forsøpling');  
+        
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Areal-og-miljo/hei-har-du-mista-noe');
+        cy.get('[class="btn-nav__title"]').eq(2).click();
+        cy.contains('Hei, har du mista noe?');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Areal-og-miljo/Areal');
+        cy.get('[class="btn-nav__title"]').eq(3).click();
+        cy.contains('Areal');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Areal-og-miljo/fiskeridirektoratet-og-fns-berekraftsmal');
+        cy.get('[class="btn-nav__title"]').eq(4).click();
+        cy.contains('Fiskeridirektoratet og FNs');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Areal-og-miljo/sameksistens');
+        cy.get('[class="btn-nav__title"]').eq(5).click();
+        cy.contains('Sameksistens');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Areal-og-miljo/Utslipp');
+        cy.get('[class="btn-nav__title"]').eq(6).click();
+        cy.contains('Utslipp');  
+
+        cy.visit('/');
+        cy.visit('http://localhost:8080/site/default/master/no/Areal-og-miljo/Dokumenter');
+        cy.get('[class="btn-nav__title"]').eq(8).click();
+        cy.contains('Dokumenterr');  
+      });
+
+      it('Test NO under menu Yrkesfiske', () => {
         cy.visit('/');
         cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske');
         cy.contains('Yrkesfiske').should('be.visible');
@@ -72,55 +165,9 @@ describe('Hjemmeside tester no', () => {
         cy.visit('http://localhost:8080/site/default/master/no/Yrkesfiske/Tema');
         cy.get('[class="btn-nav__title"]').eq(11).click();
         cy.contains('Tema');  
-
 	
       });
 
-      it('Test NO under menu Akvakultur', () => {
-        cy.visit('/');
-        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur');
-        cy.contains('Akvakultur').should('be.visible');
-           /* ==== Test meny opsjon ==== */
-        cy.visit('/');
-        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Tildeling-og-tillatelser');
-        cy.get('[class="btn-nav__title"]').eq(0).click();
-        cy.contains('Tildeling og tillatelser');        
-        
-        cy.visit('/');
-        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Drift-og-tilsyn');
-        cy.get('[class="btn-nav__title"]').eq(1).click();
-        cy.contains('Drift og tilsyn');  
-        
-        cy.visit('/');
-        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Delt-kunnskap-og-erfaring');
-        cy.get('[class="btn-nav__title"]').eq(2).click();
-        cy.contains('Delt kunnskap og erfaring');  
-
-        //cy.visit('/');
-        //cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Dokumenter');
-        //cy.get('[class="btn-nav__title"]').eq(3).click();
-        //cy.contains('Dokumenter');  
-
-        cy.visit('/');
-        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Registre-og-skjema');
-        cy.get('[class="btn-nav__title"]').eq(4).click();
-        cy.contains('Registre og skjema');  
-
-        cy.visit('/');
-        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Tall-og-analyse');
-        cy.get('[class="btn-nav__title"]').eq(5).click();
-        cy.contains('Tall og analyse');  
-
-        cy.visit('/');
-        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Tema');
-        cy.get('[class="btn-nav__title"]').eq(6).click();
-        cy.contains('Tema');  
-
-        cy.visit('/');
-        cy.visit('http://localhost:8080/site/default/master/no/Akvakultur/Akvakulturloven-og-forskrifter');
-        cy.get('[class="btn-nav__title"]').eq(7).click();
-        cy.contains('Akvakulturloven og forskrifter');  
-      });
 
 });
 
